9a223f31e231195f4d34440ed3ff17a423f0e2de
Alla Kalyakina <alla.platonova95@yandex.ru>
2022-12-10 19:45:38 +0300

    add task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,18 @@
-"use strict"
+"use strict";
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let d = Math.pow(b,2)-4*a*c;
+  switch(d > 0? 1 : (d < 0 ? -1 : 0) ) {
+    case -1:
+      break;
+    case 0:
+      arr = -b/(2*a);
+      break;
+    case 1:
+      arr[0] = (-b + Math.sqrt(d) )/(2*a);
+      arr[1] = (-b - Math.sqrt(d) )/(2*a);
+      break;
+  }
   return arr;
 }
 
