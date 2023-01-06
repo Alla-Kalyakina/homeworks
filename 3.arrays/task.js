1d01adb2adf607ec0062592d1df0fedae23927e8
Alla Kalyakina <alla.platonova95@yandex.ru>
2023-01-06 18:02:38 +0300

    add task.js

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,7 @@
 function compareArrays(arr1, arr2) {
-  
+    return arr1.length === arr2.length && arr1.every((item, index) => item === arr2[index]); 
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    let result = users.filter(user => user.gender === gender).map(agess => agess.age).reduce((acc, item, index) => (acc + item)/(index + 1), 0);
 }
