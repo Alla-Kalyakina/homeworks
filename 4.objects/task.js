df9f28e91749345f2cde0c995738df15694124a1
Alla Kalyakina <alla.platonova95@yandex.ru>
2023-01-18 23:30:52 +0300

    add task.js

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,30 @@
 function Student(name, gender, age) {
-  
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+    this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function (...marksToAdd) {
+    if(this.marks === undefined) {
+        var arr1 = [...marksToAdd];
+            this.marks = [arr1];
+        }
 }
 
 Student.prototype.getAverage = function () {
-  
+    if(this.marks === undefined || this.marks.length != 0) {
+        return Student.marks.reduce((acc, item, index, array) => {return acc += item/array.length}, 0);
+    }
+    else return 0;
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
 }
