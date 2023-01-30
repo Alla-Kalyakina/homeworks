3bac6d6f034535ec3ab0d20f51939b9049e35ac2
Alla Kalyakina <alla.platonova95@yandex.ru>
2023-01-30 15:42:17 +0300

    edit task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,20 +2,34 @@
 function solveEquation(a, b, c) {
   let arr = [];
   let d = Math.pow(b,2)-4*a*c;
-  switch(d > 0? 1 : (d < 0 ? -1 : 0) ) {
-    case -1:
-      break;
-    case 0:
-      arr = -b/(2*a);
-      break;
-    case 1:
+  if (d === 0) {
+    arr = -b/(2*a);
+  }
+  else if (d > 0) {
       arr[0] = (-b + Math.sqrt(d) )/(2*a);
       arr[1] = (-b - Math.sqrt(d) )/(2*a);
-      break;
   }
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  if(isNaN(percent)) {
+    Number(percent);
+  }
+  if(isNaN(contribution)) {
+    Number(contribution);
+  }
+  if(isNaN(amount)) {
+    Number(amount);
+  }
+  if(isNaN(countMonths)) {
+    Number(countMonths);
+  }
+  if(!isNaN(percent) && !isNaN(contribution) && !isNaN(amount) && !isNaN(countMonths)) {
+    percent *= 0.01/12;
+    let loanBody = amount - contribution;
+    let monthlyPayment = loanBody * (percent + (percent / (Math.pow((1 + percent), countMonths) - 1)));
+    return (monthlyPayment*countMonths).toFixed(2);
+  }
+  return false;
 }
